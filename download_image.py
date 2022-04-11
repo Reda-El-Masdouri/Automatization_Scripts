39be570b3432bf5bebcdef2833f224eb3ed66045
Reda E <reda.elmasdouri@gmail.com>
2022-04-11 15:56:21 +0200

    image downloaded from git repository

--- download_image.py
+++ download_image.py
@@ -1,6 +1,6 @@
 import requests
 
-image_1 = requests.get("http://git-scm.com/images/logos/downloads/Git-Logo-1788C.png")
+image_1 = requests.get("https://raw.githubusercontent.com/Reda-El-Masdouri/Automatization_Scripts/main/image.jpg")
 if image_1.status_code == 200:
     f = open("image.jpg", "wb")
     f.write(image_1.content)
